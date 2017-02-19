ed79e7a52a4b1dfe23db0ce072594598da7ea09f
nasdan <daniel.sanchez@lemoncode.net>
2017-02-19 13:37:21 +0100

    Fix webpack config and rename ts to js

--- 00 Intro/01 JSX/webpack.config.js
+++ 00 Intro/01 JSX/webpack.config.js
@@ -6,7 +6,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   context: path.join(__dirname, 'src'),
   resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.css'],
+    extensions: ['.js', '.jsx', '.css'],
   },
   entry: {
     app: './index.tsx',
@@ -26,27 +26,16 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
       },
-      // Load css from src with CSS Modules
       {
         test: /\.css$/,
-        exclude: /node_modules/,
         loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: {
             loader: 'css-loader',
           },
-        }),
-      },
-      // Load css from node_modules
-      {
-        test: /\.css$/,
-        include: /node_modules/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
         }),
       },
       // Loading glyphicons => https://github.com/gowravshekar/bootstrap-webpack
@@ -70,7 +59,7 @@ module.exports = {
     ],
   },
   // For development https://webpack.js.org/configuration/devtool/#for-development
-  devtool: 'eval-source-map',
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 8080,
